ca29c73841d307388cefb2e58d4a112e094d2208
FOXAYA <omniasaged111@gmail.com>
2024-08-10 18:20:09 +0300

    updated

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let text  = "This Is My First Task";
+// Task One>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
+
+/*let text  = "This Is My First Task";
 let i = 0;
 let output = document.querySelector("h1");
 
@@ -15,5 +17,40 @@ function typingWriter(){
     }
     setTimeout(typingWriter, 200);
 }
-typingWriter();
+typingWriter();*/
+
+
+// Task Two>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
+
+let btn_one = document.getElementById("btn-one") ;
+let btn_two =  document.getElementById("btn-two");
+let btn_three = document.getElementById("btn-three") ;
+let text = document.querySelector("h1") ;
+let back_ground = document.querySelector(".bg"); 
+let container = document.querySelector(".container")
+
+
+btn_one.addEventListener('click', () => {
+    container.insertBefore(back_ground, text);
+});
+
+
+btn_three.addEventListener('click', () => {
+    container.insertBefore(back_ground, text.nextSibling);
+});
+
+
+
+btn_two.addEventListener('click', () => {
+    
+        container.appendChild(back_ground);
+});
+
+
+
+
+
+
+
+
 
