f41258fbfb5b720375464fda5a5d64dd5288999e
FOXAYA <omniasaged111@gmail.com>
2024-08-12 13:50:52 +0300

    updated

--- main.js
+++ main.js
@@ -21,81 +21,76 @@ typingWriter();*/
 
 
 // Task Two>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
-/*
+
 let btn_one = document.getElementById("btn-one") ;
 let btn_two =  document.getElementById("btn-two");
 let btn_three = document.getElementById("btn-three") ;
 let text = document.querySelector("h1") ;
 let back_ground = document.querySelector(".bg"); 
-let container = document.querySelector(".container")
+// let container = document.querySelector(".container")
 
 
 btn_one.addEventListener('click', () => {
-    container.insertBefore(back_ground, text);
+    text.before(back_ground);
 });
 
 
 btn_three.addEventListener('click', () => {
-    container.insertBefore(back_ground, text.nextSibling);
+   text.after(back_ground);
 });
 
 
 btn_two.addEventListener('click', () => {
-    if (back_ground.parentNode === container) {
-        container.appendChild(back_ground);
-    }
-    else{
-        container.insertBefore(back_ground, text.nextSibling);
-    }
+       back_ground.append(text);
 });
-*/
+
 
 
 // Task Three>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
 
-let list = document.querySelectorAll("ul li");
-let exp = document.querySelector(".experiment");
+// let list = document.querySelectorAll("ul li");
+// let exp = document.querySelector(".experiment");
 
-// Check if backgroundImage is set in localStorage
-if (window.localStorage.getItem("backgroundImage")) {
-    // Set the background image of the experiment div
-    exp.style.backgroundImage = `url(${window.localStorage.getItem("backgroundImage")})`;
+// // Check if backgroundImage is set in localStorage
+// if (window.localStorage.getItem("backgroundImage")) {
+//     // Set the background image of the experiment div
+//     exp.style.backgroundImage = `url(${window.localStorage.getItem("backgroundImage")})`;
 
-    // Remove the active class from all list items
-    list.forEach((li) => {
-        li.classList.remove("active");
-    });
+//     // Remove the active class from all list items
+//     list.forEach((li) => {
+//         li.classList.remove("active");
+//     });
 
-    // Add the active class to the list item with the current background image
-    list.forEach((li) => {
-        if (li.dataset.back === window.localStorage.getItem("backgroundImage")) {
-            li.classList.add("active");
-        }
-    });
-}
+//     // Add the active class to the list item with the current background image
+//     list.forEach((li) => {
+//         if (li.dataset.back === window.localStorage.getItem("backgroundImage")) {
+//             li.classList.add("active");
+//         }
+//     });
+// }
 
-// Add click event listener to each list item
-list.forEach((li) => {
-    li.addEventListener("click", (e) => {
-        // Remove the active class from all list items
-        list.forEach((li) => {
-            li.classList.remove("active");
-        });
+// // Add click event listener to each list item
+// list.forEach((li) => {
+//     li.addEventListener("click", (e) => {
+//         // Remove the active class from all list items
+//         list.forEach((li) => {
+//             li.classList.remove("active");
+//         });
 
-        // Add the active class to the clicked list item
-        e.currentTarget.classList.add("active");
+//         // Add the active class to the clicked list item
+//         e.currentTarget.classList.add("active");
 
-        // Get the background image URL and set it in localStorage
-        let backgroundImage = e.currentTarget.dataset.back;
-        window.localStorage.setItem("backgroundImage", backgroundImage);
+//         // Get the background image URL and set it in localStorage
+//         let backgroundImage = e.currentTarget.dataset.back;
+//         window.localStorage.setItem("backgroundImage", backgroundImage);
 
-        // Change the background image of the experiment div
-        exp.style.backgroundSize = "cover";
+//         // Change the background image of the experiment div
+//         exp.style.backgroundSize = "cover";
 
-        exp.style.backgroundImage = `url(${backgroundImage})`;
+//         exp.style.backgroundImage = `url(${backgroundImage})`;
  
-    });
-});
+//     });
+// });
 
 
 
